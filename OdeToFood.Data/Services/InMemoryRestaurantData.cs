6790ede9a192293846b93ea966726c9df869bcbe
Luis.Pedro.Fonseca <Luis.Pedro.Fonseca@outlook.com>
2019-11-23 18:22:20 +0000

    Add project files.

---
+++ OdeToFood.Data/Services/InMemoryRestaurantData.cs
@@ -0,0 +1,26 @@
+﻿using OdeToFood.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data.Services
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant { Id = 1, Name = "Lu Pizza", Cuisine = CuisineType.Italian},
+                new Restaurant { Id = 2, Name = "Bifanas do Ze", Cuisine = CuisineType.Portuguese},
+                new Restaurant { Id = 3, Name = "Croissant du soleil", Cuisine = CuisineType.French}
+            };
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return restaurants.OrderBy(r => r.Name);
+        }
+    }
+}
